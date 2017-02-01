5a416e54fd3ea3b6790ebdd4909057d387ed04f6
camilohel777 <camilo.rive007@gmail.com>
2017-02-01 17:46:23 -0500

    comment section works

--- front_end/app.js
+++ front_end/app.js
@@ -1,6 +1,30 @@
 var app = angular.module('flapperNews',['ui.router']);
 // Using ui.router to allow the app to work in a SPA (single-page application)
 
+// Change url based on state(like a state machine)
+// Configuring our ui-router. $stateProvider and $urlRouterProvider used to set up home router and home state
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider)
+  {
+    $stateProvider // :
+    // States and their nested views
+                  .state('home', {
+                  url: '/home',
+                  templateUrl: '/home.html',
+                  controller: 'MainCtrl'
+                })
+    // The {id} is a route parameter that will be available to the controller
+                .state('posts', {
+                  url: '/posts/{id}',
+                  templateUrl: '/posts.html',
+                  controller: 'PostsCtrl'
+                });
+
+      $urlRouterProvider.otherwise('home');
+}]);
+
 //Factory Also known as service used to hold the information
 app.factory('posts',[function(){
   var o = {
@@ -12,9 +36,7 @@ app.factory('posts',[function(){
 app.controller('MainCtrl', [ '$scope', 'posts',
 function($scope, posts)
 {
-  $scope.test = 'Hello World';
   $scope.posts = posts.posts;
-
   $scope.addPost = function()
   {
     if(!$scope.title || $scope.title === '')
@@ -31,35 +53,13 @@ function($scope, posts)
         {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
       ]
     });
-
     $scope.title = '';
     $scope.link = '';
-    $scope.incrementUpvotes = function (post){
-      post.upvotes += 1;
-    };
-  };
-}]);
-
-// Change url based on state(like a state machine)
-// Configuring our ui-router. $stateProvider and $urlRouterProvider used to set up home router and home state
-app.config(['$stateProvider','$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider)
+  }
+  $scope.incrementUpvotes = function (post)
   {
-    $stateProvider //:
-    // States and their nested views
-                  .state('home', {
-                  url: '/home',
-                  templateUrl: '/home.html',
-                  controller: 'MainCtrl'
-                })
-    // The {id} is a route parameter that will be available to the controller
-                .state('posts', {
-                  url: '/posts/{id}',
-                  templateURL: '/posts.html',
-                  controller: 'PostsCtrl'
-                });
-
-      $urlRouterProvider.otherwise('home');
+    post.upvotes += 1;
+  }
 }]);
 
 app.controller('PostsCtrl', [
@@ -81,5 +81,9 @@ app.controller('PostsCtrl', [
         upvotes: 0
       });
       $scope.body = '';
+    };
+    $scope.incrementUpvotes = function (comment)
+    {
+        comment.upvotes += 1;
     }
   }]);
