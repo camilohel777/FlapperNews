0dcdb00c13563a7a3ca04a3b3f5002cbd7f6a8cf
camilohel777 <camilo.rive007@gmail.com>
2017-02-04 12:04:39 -0500

    GET and POST updated

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -7,3 +7,29 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// Creating GET route for retrieving posts, returns JSON list containing all posts
+var mongoose = require('mongoose'); // Make sure mongoose is imported
+var Post = mongoose.model('Post'); // Get Handles to Post and Comment models
+var Comment = mongoose.model('Comment');
+
+// Use express get()  ethod to define URL for route (/posts)
+router.get('/posts', function(req, res, next) { // Function to handle requests (req :contains info)(res: respond to client)
+  Post.find(function(err, posts){  // Query database for all posts
+    if(err){ return next(err); } // Pass error to error-handling function
+
+    res.json(posts); // Send retrieved posts back to client
+  });
+});
+
+// Creating a POST route for creating posts
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post)
+  {
+    if(err){ return next(err); };
+    
+    res.json(post);
+  });
+});
